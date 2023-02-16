7dedc525cc9560678f77d2e2def1b8bd4281d751
wem666 <williamdahlstrand@protonmail.com>
2023-02-16 21:45:56 +0100

    Added verbose mode and improved usage message

--- cpyio.sh
+++ cpyio.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-# Check if the input file and output directory were provided
+# Set up command line argument parsing
+while getopts ":v" opt; do
+  case $opt in
+    v)
+      verbose=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+# Check for required arguments
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 [input_file] [output_directory]"
-    exit 1
+  echo "Usage: $0 [input_file] [output_directory]"
+  echo "  -v  Verbose mode. Print a message after the file is copied."
+  echo ""
+  echo "Copy an input file to an output directory. If the output directory does not exist, it will be created."
+  exit 1
 fi
 
-# Get the input file and output directory from the command line
+# Get input file and output directory
 input_file=$1
-output_dir=$2
+output_directory=$2
 
-# Loop through each file in the output directory
-for file in "$output_dir"/*; do
-    # Check if the file is a regular file (not a directory or a symlink)
-    if [ -f "$file" ]; then
-        # Copy the contents of the input file to the output file
-        cp "$input_file" "$file"
-    fi
-done
+# Make sure input file exists
+if [ ! -f "$input_file" ]; then
+  echo "Error: Input file '$input_file' does not exist."
+  exit 1
+fi
+
+# Make sure output directory exists, create it if not
+if [ ! -d "$output_directory" ]; then
+  mkdir -p "$output_directory"
+fi
+
+# Copy input file to output directory
+cp "$input_file" "$output_directory"
+
+# Print success message
+if [ "$verbose" = true ]; then
+  echo "Successfully copied '$input_file' to '$output_directory'."
+else
+  echo "Success!"
+fi
